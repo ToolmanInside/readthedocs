369b1dee3cf4e1489e49d45b2cd2fec6492460cf
jiamingns <yejiaming20@126.com>
2019-10-02 15:48:34 +0800

    commit

--- docs/conf.py
+++ docs/conf.py
@@ -201,7 +201,7 @@ htmlhelp_basename = ''
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
+# latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
 #'papersize': 'letterpaper',
 
@@ -210,14 +210,14 @@ latex_elements = {
 
 # Additional stuff for the LaTeX preamble.
 #'preamble': '',
-}
+# }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
+# latex_documents = [
         
-]
+# ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
